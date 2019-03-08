1f60bdfd37422b83404fea7344f45b162a4e7467
Aditya <adityaprasanna24@gmail.com>
2019-03-08 22:00:09 +0530

    pointing to server prod

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -13,8 +13,8 @@ import { Observable, throwError } from 'rxjs';
 
 export class AppService {
 
-  // private _organizationUrl = "http://www.festify.in:8000/api/";
-  private _organizationUrl = "http://localhost:8000/api/";
+  private _organizationUrl = "http://www.festify.in:8000/api/";
+  // private _organizationUrl = "http://localhost:8000/api/";
 
   constructor(private http: Http, private _router: Router) { }
 
