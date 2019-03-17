a5dc5262720fc1138761dd9c9fd1059e092225e5
Swapnil Kumar <swapnil.kumar@myntra.com>
2019-03-17 12:33:44 +0530

    my trial

--- src/app/components/organization-dashboard/organization-dashboard.component.ts
+++ src/app/components/organization-dashboard/organization-dashboard.component.ts
@@ -8,7 +8,7 @@ declare var $: any;
 @Component({
   selector: 'app-organization-dashboard',
   templateUrl: './organization-dashboard.component.html',
-  styleUrls: ['./organization-dashboard.component.sass']
+  styleUrls: ['./organization-dashboard.component.scss']
 })
 
 export class OrganizationDashboardComponent implements OnInit {
