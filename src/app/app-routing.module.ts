02467ad8b36ba4b8b7bc06dea491904a981fffd0
Aditya Prasanna <adityaprasanna@Adityas-MacBook-Pro.local>
2019-05-29 23:19:13 +0530

    form update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,99 +1,99 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AuthGuardService } from './auth-guard.service';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { OrganizationsComponent } from './components/organizations/organizations.component';
-import { FestUploadFormComponent } from './components/fest-upload-form/fest-upload-form.component';
-import { OrganizationDashboardComponent } from './components/organization-dashboard/organization-dashboard.component';
-import { FestComponent } from './components/fest/fest.component';
-import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
-import { EditorgdashboardComponent } from './components/editorgdashboard/editorgdashboard.component';
-import { EditfestComponent } from './components/editfest/editfest.component';
-import { AftersuccessComponent } from './components/aftersuccess/aftersuccess.component';
-import { FailureComponent } from './components/failure/failure.component';
-import { PaymentComponent } from './components/payment/payment.component';
-import { FestspecificComponent } from './components/festspecific/festspecific.component';
-import { OrgauthService } from './orgauth.service';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { AuthGuardService } from "./auth-guard.service";
+import { RegistrationComponent } from "./components/registration/registration.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { OrganizationsComponent } from "./components/organizations/organizations.component";
+import { FestUploadFormComponent } from "./components/fest-upload-form/fest-upload-form.component";
+import { MunUploadFormComponent } from "./components/mun-upload-form/mun-upload-form.component";
+import { OrganizationDashboardComponent } from "./components/organization-dashboard/organization-dashboard.component";
+import { FestComponent } from "./components/fest/fest.component";
+import { PagenotfoundComponent } from "./components/pagenotfound/pagenotfound.component";
+import { EditorgdashboardComponent } from "./components/editorgdashboard/editorgdashboard.component";
+import { EditfestComponent } from "./components/editfest/editfest.component";
+import { AftersuccessComponent } from "./components/aftersuccess/aftersuccess.component";
+import { FailureComponent } from "./components/failure/failure.component";
+import { PaymentComponent } from "./components/payment/payment.component";
+import { FestspecificComponent } from "./components/festspecific/festspecific.component";
+import { OrgauthService } from "./orgauth.service";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomeComponent
   },
   {
-    path: 'home',
+    path: "home",
     component: HomeComponent
   },
   {
-    path: 'signup',
+    path: "signup",
     component: RegistrationComponent
   },
   {
-    path: 'userdashboard',
+    path: "userdashboard",
     component: DashboardComponent,
     canActivate: [AuthGuardService]
   },
   {
-    path: 'organizations',
-    component: OrganizationsComponent,
+    path: "organizations",
+    component: OrganizationsComponent
   },
   {
-    path: 'fest-upload-form',
-    component: FestUploadFormComponent,
+    path: "fest-upload-form",
+    component: MunUploadFormComponent,
     canActivate: [OrgauthService]
   },
   {
-    path: 'orgdashboard',
+    path: "orgdashboard",
     component: OrganizationDashboardComponent,
     canActivate: [OrgauthService]
   },
   {
-    path: 'edit',
+    path: "edit",
     component: EditorgdashboardComponent,
     canActivate: [OrgauthService]
   },
   {
-    path: 'editfest',
+    path: "editfest",
     component: EditfestComponent,
     canActivate: [OrgauthService]
   },
   {
-    path: 'success/:&fname:&lname:&email:&phone:&fest:&event:&price:&tid',
-    component: AftersuccessComponent,
+    path: "success/:&fname:&lname:&email:&phone:&fest:&event:&price:&tid",
+    component: AftersuccessComponent
   },
   {
-    path: 'failure',
+    path: "failure",
     component: FailureComponent,
     canActivate: [AuthGuardService]
   },
   {
-    path: 'fest',
+    path: "fest",
     component: FestComponent
   },
   {
-    path: 'festDetails/:id',
-    component: FestspecificComponent,
+    path: "festDetails/:id",
+    component: FestspecificComponent
   },
   {
-    path: 'payment',
+    path: "payment",
     component: PaymentComponent,
     canActivate: [AuthGuardService]
   },
   {
-    path: 'festspecific',
-    component: FestspecificComponent,
+    path: "festspecific",
+    component: FestspecificComponent
   },
   {
-    path: '**',
+    path: "**",
     component: PagenotfoundComponent
   }
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/mun-upload-form/mun-upload-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-mun-upload-form",
+  templateUrl: "./mun-upload-form.component.html",
+  styleUrls: ["./mun-upload-form.component.sass"]
+})
+export class MunUploadFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
